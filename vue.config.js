6b9b4685e39ae000c261b2458e2087089c7b1846
a3510377 <a102009102009@gmail.com>
2021-08-03 14:03:08 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@
 module.exports = {
     outputDir: 'docs',
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/github-pages-vue-cli/' : '/',
+        ? '/rpmtw-pages-vue-cli/' : '/',
 }
