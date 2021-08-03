d49e7997b85c7f8f31cf3c40dd15ec2670abd01a
a3510377 <a102009102009@gmail.com>
2021-08-03 16:58:19 +0800

    Better update

--- .cmd
+++ .cmd
@@ -3,7 +3,7 @@ copy %~dp0docs\index.html %~dp0docs\404.html
 
 git init
 git add .
-git commit -m "更好的的更新"
+git commit -m "Better update"
 git branch -M main
 git remote add origin https://github.com/a3510377/rpmtw-pages-vue-cli.git
 git push -u origin main
