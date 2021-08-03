9f3da3f08bcdb4a847e861fc7c0e5f5e5aa0b399
a3510377 <a102009102009@gmail.com>
2021-08-03 16:57:03 +0800

    ?游末???湔

--- .cmd
+++ .cmd
@@ -1,10 +1,9 @@
 @echo off
-cls
 copy %~dp0docs\index.html %~dp0docs\404.html
 
 git init
 git add .
-git commit -m "first commit"
+git commit -m "更好的的更新"
 git branch -M main
 git remote add origin https://github.com/a3510377/rpmtw-pages-vue-cli.git
 git push -u origin main
