3baf185ea01ef87044c1d91976e082cbfa32eece
a3510377 <a102009102009@gmail.com>
2021-08-04 12:59:41 +0800

    Better update

---
+++ public/main.js
@@ -0,0 +1,3 @@
+/* eslint-disable-next-line no-undef */
+$(function () { let title = document.title; $(document).on("visibilitychange", () => { switch (document.visibilityState) { case "hidden": (document.title = "你看不到我" || ""), setTimeout(() => "hidden" === document.visibilityState && (document.title = title), 1e3); break; case "visible": (document.title = "歡迎回來" || ""), setTimeout(() => "visible" === document.visibilityState && (document.title = title), 1e3); break; } }) })
+/* 此專案使用 vue cli 前端框架建置 */
