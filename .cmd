528fc14afcba6bc5dbe729c2291b90eee84cea1d
a3510377 <a102009102009@gmail.com>
2021-08-05 08:45:51 +0800

    建置 Wiki 測試

--- .cmd
+++ .cmd
@@ -4,7 +4,7 @@ copy %~dp0docs\index.html %~dp0docs\404.html
 
 git init
 git add .
-git commit -m "Better update"
+git commit -m "建置 Wiki 測試"
 git branch -M main
 git remote add origin https://github.com/RPMTW/RPMTW-Website.git
 git push -u origin main
