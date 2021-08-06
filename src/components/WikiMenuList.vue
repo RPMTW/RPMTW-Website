1735de0220e9d1d7c53cfd3fea9c94e987af1be8
a3510377 <a102009102009@gmail.com>
2021-08-06 15:06:10 +0800

    >>>

--- src/components/WikiMenuList.vue
+++ src/components/WikiMenuList.vue
@@ -10,7 +10,7 @@
                     <h4 @click="showLi">{{ data.title }}</h4>
                 </router-link>
                 <li v-for="_data in data.names" :key="_data" class="liHref">
-                    <router-link class="menuList_" :href="`/Wiki${data.url}/#${_data.url}`">{{ _data.title }}</router-link>
+                    <router-link class="menuList_" :to="`/Wiki${data.url}/#${_data.url}`">{{ _data.title }}</router-link>
                 </li>
             </ul>
         </div>
