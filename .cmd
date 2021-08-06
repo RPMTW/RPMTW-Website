e9a7dfe16b1877010dfd1ef0fb886edba71bea9f
a3510377 <a102009102009@gmail.com>
2021-08-06 12:12:03 +0800

    >>>

--- .cmd
+++ .cmd
@@ -3,7 +3,7 @@ copy "%~dp0\docs\index.html" "%~dp0\docs\404.html"
 
 git init
 git add .
-git commit -m "添加 Wiki 協助翻譯頁"
+git commit -m ">>>"
 git branch -M main
 git remote add origin https://github.com/RPMTW/RPMTW-Website.git
 git push -u origin main
