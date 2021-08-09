4f7fb7c9325c303f38865d10223d66a9218d7a6f
a3510377 <a102009102009@gmail.com>
2021-08-09 11:07:15 +0800

    set aria-label css

--- .cmd
+++ .cmd
@@ -3,7 +3,7 @@ copy "%~dp0\docs\index.html" "%~dp0\docs\404.html"
 
 git init
 git add .
-git commit -m "set home-main-img css"
+git commit -m "set aria-label css"
 git branch -M main
 git remote add origin https://github.com/RPMTW/RPMTW-Website.git
 git push -u origin main
