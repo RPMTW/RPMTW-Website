59f3dd73d90df42d94907f578a69f43cc24e4f89
a3510377 <a102009102009@gmail.com>
2021-08-09 08:32:14 +0800

    up meta

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import jQuery from 'jquery'
 import store from './store'
 
-window.$ = window.jQuery = jQuery
-
-
-
 createApp(App)
+    .use(store)
     .use(store)
     .use(router)
     .mount('#app')
