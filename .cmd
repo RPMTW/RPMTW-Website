c160fcb3d1aa12f5de481b29ac9a19b308969a4f
a3510377 <a102009102009@gmail.com>
2021-08-09 11:27:21 +0800

    >>>

--- .cmd
+++ .cmd
@@ -1,9 +1,2 @@
 @echo off
-copy "%~dp0\docs\index.html" "%~dp0\docs\404.html"
-
-git init
-git add .
-git commit -m "set aria-label css"
-git branch -M main
-git remote add origin https://github.com/RPMTW/RPMTW-Website.git
-git push -u origin main
+yarn build & copy "%~dp0\docs\index.html" "%~dp0\docs\404.html"
