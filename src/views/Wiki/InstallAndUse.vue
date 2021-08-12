f4d45893b24e97ce3da03dbb167a70c6f126a8b2
a3510377 <a102009102009@gmail.com>
2021-08-12 11:58:05 +0800

    修復: ....我是天才，沒改道字...

--- src/views/Wiki/InstallAndUse.vue
+++ src/views/Wiki/InstallAndUse.vue
@@ -3,10 +3,9 @@
     <div class="content">
       <section class="section" id="how-install">
         <h1>如何安裝RPMTW繁體中文自動更新模組?</h1>
-        <p>
-          可以，要修改設定只要按下預設快捷鍵 O
-          (可自行修改)，就會打開一個設定選單。
-        </p>
+        <router-link to="/install/version"
+          >&lt;&lt;點我查看教學&gt;&gt;</router-link
+        >
       </section>
       <section class="section" id="if-game-crashes">
         <h1>遊戲因為裝了這個模組崩潰了怎麼辦?</h1>
