1eb30ae41cfe18a6e1ba730a2cdd426c551f819a
a3510377 <a102009102009@gmail.com>
2021-08-15 12:21:18 +0800

    test

--- src/main.js
+++ src/main.js
@@ -2,14 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import VueGtag from "vue-gtag-next";
 
 createApp(App)
     .use(store)
     .use(router)
-    .use(VueGtag, {
-        property: {
-            id: "G-JS3XKB3H80"
-        }
-    })
     .mount('main')
