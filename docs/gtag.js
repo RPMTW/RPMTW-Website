28e217ff418ca016380b8b76e1620946cf4bb917
a3510377 <a102009102009@gmail.com>
2021-08-15 12:26:27 +0800

    Delete gtag.js

--- docs/gtag.js
+++
@@ -1,9 +0,0 @@
-< !--Global site tag(gtag.js) - Google Analytics-- >
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-JN1EYJK7XV"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-JN1EYJK7XV');
-</script>
