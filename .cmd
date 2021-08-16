6acf4fdd44bd0fbd093d64ed6e98f488728e7359
a3510377 <a102009102009@gmail.com>
2021-08-16 16:25:32 +0800

    ..... .gitignore 怪怪的...

--- .cmd
+++ .cmd
@@ -1,2 +1,2 @@
 @echo off
-npm run build& copy "docs\index.html" "docs\404.html" & echo www.rpmtw.ga >> "docs\CNAME"
+npm run build & copy "docs\index.html" "docs\404.html" & echo www.rpmtw.ga >> "docs\CNAME"
