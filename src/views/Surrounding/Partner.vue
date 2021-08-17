0b9d175ef6bedcfdd604dc0af5b7af92aaed5796
a3510377 <a102009102009@gmail.com>
2021-08-17 21:49:22 +0800

    合作夥伴資料化

--- src/views/Surrounding/Partner.vue
+++ src/views/Surrounding/Partner.vue
@@ -2,137 +2,23 @@
   <div id="Partner" class="flex">
     <h1>合作夥伴</h1>
     <div class="flex partner">
-      <div class="unit flex">
+      <div class="unit flex" v-for="i in data" :key="i">
         <div class="flex name">
-          <img src="https://www.dreamcity.studio/images/logo_0218.png" alt="" />
-          <h2 class="partner-Name">夢都 - 夢想之都工作室</h2>
+          <img :src="i.img" alt="" />
+          <h2 class="partner-Name">{{ i.name }}</h2>
         </div>
         <div class="description flex">
           <div class="ctx flex flex-down flex-item-center">
-            <h3>簡介</h3>
+            <h3>{{ i.data.title }}</h3>
             <p>
-              2015年，來自臺灣、香港與澳門，擁有相同志向的學生們，組成了夢想之都工作室。<br />
-              我們深耕於Minecraft地圖製作，致力於創作並推廣Minecraft。<br />
-              夢都集結了各方設計、美術、資訊人才，在創作的過程中，也藉此磨練專長與團隊共同成長。<br />
-              期望廣大玩家能在我們的地圖中獲得良好的遊戲體驗。<br />
-            </p>
-            <div class="flex">
-              <a href="https://www.dreamcity.studio/" class="btn">官方網站</a>
-              <a href="https://discord.gg/2Eq4zBg4En" class="btn"
-                >Discord 討論區</a
-              >
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="unit flex">
-        <div class="flex name">
-          <img
-            src="https://yt3.ggpht.com/ytc/AKedOLTGzEb1FIXJBdfI7m_PLfiG-DoC3vF3CwFQFAg0=s88-c-k-c0x00ffffff-no-rj"
-            alt=""
-          />
-          <h2 class="partner-Name">舞秋風</h2>
-        </div>
-        <div class="description flex">
-          <div class="ctx flex flex-down flex-item-center">
-            <h3>簡介</h3>
-            <p>
-              大家好 我是舞秋風<br />
-              頻道中會有遊戲實況<br />
-              舉凡有趣的遊戲 都有可能實況一下<br />
-              不論是小遊戲、好遊戲、爛遊戲、老遊戲<br />
-              只是要想玩的都有可能實況<br />
-              有時還會有一些雜七雜八的影片或真人演出<br />
-              如果喜歡秋風的頻道別忘了訂閱喔<br />
-              有空時 還請多多留言<br />
-              給我最直接的反應與回饋<br />
-            </p>
-            <div class="flex">
-              <a href="https://www.youtube.com/user/MrChesterccj" class="btn"
-                >Youtube</a
-              >
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="unit flex">
-        <div class="flex name">
-          <img
-            src="https://yt3.ggpht.com/ytc/AKedOLSxwnB7Y5LA1BwnCX46hz8MrGErHr49mi-FWQgbnNg=s88-c-k-c0x00ffffff-no-rj"
-            alt=""
-          />
-          <h2 class="partner-Name">紙片模型</h2>
-        </div>
-        <div class="description flex">
-          <div class="ctx flex flex-down flex-item-center">
-            <h3>簡介</h3>
-            <p>
-              "既然當不了選手，那就當個好教練吧！"<br />
-              雖然玩麥塊比不上別人，但自認非常喜歡研究模組。<br />
-              如果你也喜歡打造屬於自己的遊戲，歡迎加入我們的大家庭。相信這裏總有一款屬於你的模組！<br />
+              {{ i.data.text }}
             </p>
             <div class="flex">
               <a
-                href="https://www.youtube.com/c/%E7%B4%99%E7%89%87%E6%A8%A1%E5%9E%8B"
-                class="btn"
-                >Youtube</a
-              >
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="unit flex">
-        <div class="flex name">
-          <img src="https://yt3.ggpht.com/ytc/AKedOLR74DlW1-5CXcUgMOiCp2ntLmSDex9yrOoNaY8K=s88-c-k-c0x00ffffff-no-rj" alt="" />
-          <h2 class="partner-Name">白凱吉</h2>
-        </div>
-        <div class="description flex">
-          <div class="ctx flex flex-down flex-item-center">
-            <h3>簡介</h3>
-            <p>
-              嗨 大家好 我是小白 我是個遊戲實況主<br />
-              喜歡我的影片的話，也歡迎持續關注頻道哦<br />
-              直播紀錄都會在這裡重播<br />
-              https://www.youtube.com/channel/UCFU8Pd8D8r2QWbK7--RIYcQ<br />
-              謝謝你們的支持 也謝謝你們的訂閱!<br />
-              有時會實況創世以外的單機遊戲<br />
-              其他系列的影片<br />
-              也希望大家可以 看看喔<br />
-            </p>
-            <div class="flex">
-              <a
-                href="https://www.youtube.com/channel/UC0wE4yLmbEJVVKHBIHOyiNA"
-                class="btn"
-                >Youtube</a
-              >
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="unit flex">
-        <div class="flex name">
-          <img
-            src="https://yt3.ggpht.com/ytc/AKedOLQvPW84OkL-RIq_ba4UEUeLcisW4KvcuXEWiHId=s88-c-k-c0x00ffffff-no-rj"
-            alt=""
-          />
-          <h2 class="partner-Name">小白</h2>
-        </div>
-        <div class="description flex">
-          <div class="ctx flex flex-down flex-item-center">
-            <h3>簡介</h3>
-            <p>
-              嗨 大家好 我是小白 我是個遊戲實況主<br />
-              喜歡我的影片的話，也歡迎持續關注頻道哦<br />
-              謝謝你們的支持 也謝謝你們的訂閱!<br />
-              有時會實況創世以外的單機遊戲<br />
-              其他系列的影片<br />
-              也希望大家可以 看看喔<br />
-            </p>
-            <div class="flex">
-              <a
-                href="https://www.youtube.com/c/asd0951958683WhiteGx/about"
-                class="btn"
-                >Youtube</a
+                :href="button.url"
+                v-for="button in i.data.buttons"
+                :key="button"
+                >{{ button.title }}</a
               >
             </div>
           </div>
@@ -151,11 +37,13 @@
 <script>
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
+import data from "@/data/Partner.json";
 export default {
   name: "Partner",
   data() {
     return {
       htmlUnit: false,
+      data: data,
     };
   },
   methods: {},
