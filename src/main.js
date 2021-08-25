a2efd0020d6bd3aa3a3695450ef9548235182db9
a3510377 <a102009102009@gmail.com>
2021-08-25 19:25:12 +0800

    確認造成生成錯誤之插件

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
 
 createApp(App)
-    // .use(VueSocketio, socketio("https://cosmic-chat-server.siongsng.repl.co/"))
     .use(store)
     .use(router)
     .mount('main')
