dfd353530d424f34c733b3b1cffc7eb608d513c8
a3510377 <a102009102009@gmail.com>
2021-08-26 19:12:53 +0800

    debug

--- src/router/index.js
+++ src/router/index.js
@@ -125,18 +125,6 @@ const routes = [
         path: "/Translator",
         name: "Translator",
         component: () => import("@/views/Surrounding/crowdin/Crowdin.vue"),
-        /*
-        meta: {
-            breadcrumb: "Translator",
-        },
-        children: [
-             {
-                 path: "",
-                 components: {
-                     content: () => import("@/views/Surrounding/crowdin/GetMods.vue")
-                 }
-             }
-         ] */
     }, {
         path: "/CosmicChat",
         name: "CosmicChat",
